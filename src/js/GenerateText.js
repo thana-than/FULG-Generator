e755c6f13586e9d16da76d38d536e34e61524391
Than <thanthemannn@gmail.com>
2025-04-07 17:24:09 -0400

    added profanity filter

--- src/js/GenerateText.js
+++ src/js/GenerateText.js
@@ -1,20 +1,42 @@
 import tracery from 'tracery-grammar';
+import { Profanity } from '@2toad/profanity';
 
-const grammar = tracery.createGrammar(require('../.generated/wordData.json'));
+const wordData = require('../.generated/wordData.json');
+
+const grammar = tracery.createGrammar(wordData);
 
 grammar.addModifiers(tracery.baseEngModifiers);
 
+const profanity = new Profanity();
+
+//* Add our existing words to the profanity whitelist because we probably want to say them
+const safewords = []
+for (let key in wordData) {
+    const column = wordData[key];
+    for (let phrase in column) {
+        const words = column[phrase].split(' ');
+        safewords.push(...words);
+    }
+}
+profanity.removeWords(safewords);
+
+function get(root) {
+    let str;
+    do {
+        str = grammar.flatten(root);
+    }
+    while (profanity.exists(str));
+    return str;
+}
+
 export function GenerateName() {
-    var value = grammar.flatten("#nameRoot#");
-    return value;
+    return get("#nameRoot#");
 }
 
 export function GenerateTrait() {
-    var value = grammar.flatten("#traitRoot#");
-    return value;
+    return get("#traitRoot#");
 }
 
 export function GenerateSlogan() {
-    var value = grammar.flatten("#sloganRoot#");
-    return value;
+    return get("#sloganRoot#");
 }
